64ba3621893cd832c64903f10e6ecbc6f6051f17
vjekop <sydneyphillips2005@gmail.com>
2026-04-20 15:53:50 -0400

    update stripe api version to 2025-02-24.acacia

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -2,7 +2,7 @@ import Stripe from 'stripe';
 import { NextRequest, NextResponse } from 'next/server';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2023-10-16',
+  apiVersion: '2025-02-24.acacia',
 });
 
 const products = [
